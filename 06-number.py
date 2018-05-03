0d644d80a72b3d19edb78b3cc2f6de0a6f15adec
nilwhiz <33026956+nilwhiz@users.noreply.github.com>
2018-05-03 22:49:14 +0800

    SCRAPPED

---
+++ 06-number.py
@@ -0,0 +1,27 @@
+'''
+Variables can also hold numeric values.
+The simplest kind of number in Python
+is the integer, which is a whole number with no decimal point.
+'''
+
+int1 = 5
+int2 = 10
+int3 = -15
+
+'''
+A number with a decimal point is called a float.
+You can define floats with numbers after
+the decimal point or by just including a decimal point at the end.
+'''
+
+float1 = 5.0
+float2 = 20.
+float3 = -3.5
+
+'''
+You can also define a float using
+scientific notation, with e indicating the power of 10.
+'''
+
+# this evaluates to 120:
+float4 = 1.2e2
