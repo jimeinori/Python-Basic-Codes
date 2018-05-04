4ee54d66262d35f9662b7efd479006417173a215
nilwhiz <33026956+nilwhiz@users.noreply.github.com>
2018-05-04 18:43:00 +0800

    One

---
+++ 08-multiline-strings.py
@@ -0,0 +1,10 @@
+#If we want a string to span multiple lines, we can also use triple quotes.
+#""" or '''
+#This address spans multiple lines,
+#and is still contained in one variable, address_string.
+
+address_string = """We are the
+powerful heroes
+in the world of fondness."""
+
+print(address_string)
