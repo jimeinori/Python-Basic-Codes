d6860557e456929ef801edeb91c61a1707346c8d
nilwhiz <33026956+nilwhiz@users.noreply.github.com>
2018-05-04 18:51:34 +0800

    One

---
+++ 09-booleans.py
@@ -0,0 +1,24 @@
+'''
+Sometimes we have a need for variables
+that are either true or false.
+This datatype, which can only ever
+take one of two values, is called a boolean.
+'''
+
+a = True
+b = False
+
+'''
+A boolean is actually a special case of an integer.
+A value of True corresponds to an integer value of 1,
+and will behave the same.
+A value of False corresponds to an integer value of 0.
+'''
+
+#Example
+
+you_are_robot = True
+print("Hello There, I'm a robot.")
+print("This application will confirm the truth.")
+print("Are you a robot?")
+print(you_are_robot)
