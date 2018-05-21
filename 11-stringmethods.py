94d64a6c45f86957d13107dbf49bd2cf9aa1387f
nilwhiz <33026956+nilwhiz@users.noreply.github.com>
2018-05-22 03:35:04 +0800

    Two

---
+++ 11-stringmethods.py
@@ -0,0 +1,38 @@
+#String methods let you perform specific tasks for strings.
+
+'''
+
+    len() - gets the length (the number of characters) of a string.
+    lower() - you can use this to get rid of all the capitalization.
+    upper() - makes your string all in capital letters.
+    str() -  turns non-strings into strings.
+'''
+
+#len() example:
+
+dog = "Chow Chow"
+print(len(dog))
+
+#lower() example:
+
+fish = "Bank Sea Bass"
+print(fish.lower())
+
+#upper() example:
+
+meal = "adobong manok"
+print(meal.upper())
+
+#str() example:
+
+yourmoney = 100.25
+print(str(yourmoney))
+
+'''
+DOT NOTATION:
+Methods that use dot notation only work with strings.
+
+On the other hand, len() and str() can work on other data types.
+'''
+print("---------------------")
+input("Press ENTER to exit.")
