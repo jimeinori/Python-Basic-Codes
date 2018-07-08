ec303cdb6050cad7d22ebf68664f897866468171
公理 <mxnventura@protonmail.com>
2018-07-08 18:06:52 +0800

    Four

---
+++ 14-conditions.py
@@ -0,0 +1,28 @@
+#==================={Conditions/ControlFlow}=====================#
+
+#Control flow - gives us this ability to choose among outcomes.
+
+#ExampleCodeWithoutContext
+def yourself():
+	print("You're now trapped inside a box.")
+	print("Will you stay inside or go outside?")
+	
+	answer = input("Type inside or outside and hit 'Enter'.").lower()
+
+	if answer == "inside" or answer == "i":
+		print("---------------------")
+		print("You suffocated yourself and die.")
+		print("---------------------")
+		input("Press ENTER to exit.")
+	elif answer == "outside" or answer == "o":
+		print("---------------------")
+		print("It's very dark and cold outside, you can't see anything")
+		print("until a blunt object hit your head and you died instantly.")
+		print("---------------------")
+		input("Press ENTER to exit.")
+	else:
+		print("---------------------")
+		print("Please make your choice correctly.")
+		yourself()
+
+yourself()
